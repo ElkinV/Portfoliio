abe5b172630545792f7a5a44b10554f64eac0252
Elkin Vasquez <73296467+ElkinV@users.noreply.github.com>
2023-07-24 21:42:45 +0000

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/imagee.jpg";
+import image from "../images/image.jpg";
 import "./css/about.css";
 
 const imageAltText = "rose abstract background";
